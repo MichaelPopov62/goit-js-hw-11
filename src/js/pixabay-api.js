8de1c83c4a3807ffe32f0203fc86e4a939e67a47
MichaelPopov62 <romantikzizni@gmail.com>
2025-05-14 22:25:40 +0300

    add changed css js html

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,29 @@
 //Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
 
-getImagesByQuery(query);
+// getImagesByQuery(query);
+
+import axios from 'axios';
+
+const API_KEY = '50290691-d51719558e36c91c48babc26d'; // свій API-ключ.
+const BASE_URL = 'https://pixabay.com/api/';
+
+/**
+ * Виконує запит до Pixabay API для пошуку зображень за ключовим словом.
+ * @param {string} query - Пошуковий запит.
+ * @returns {Promise<Object>} - Дані відповіді API.
+ */
+export async function getImagesByQuery(query) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
+    query
+  )}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  }
+  catch (error) {
+    console.error('Помилка під час виконання HTTP-запиту:', error.message); 
+    throw new Error('Помилка завантаження даних з Pixabay.');
+  }
+}
+
--- src/main.js
+++ src/main.js
@@ -3,3 +3,45 @@
 //Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі.
 
 //Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showErrorMessage,
+  showSuccessMessage,
+} from './js/render-functions.js';
+
+const form = document.querySelector('.form');
+const searchInput = form.querySelector('input[name="search-text"]');
+// подія на сабміт форми
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const query = searchInput.value.trim();
+
+  // Перевірка на порожній рядок
+  if (!query) {
+    alert('Будь ласка, введіть пошуковий запит.');
+    return;
+  }
+// очищення галереі передтновим запитом
+  clearGallery();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    // Якщо порожній масив
+    if (data.hits.length === 0) {
+      showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
+    } else {
+      //створюю галерею
+      createGallery(data.hits);
+      showSuccessMessage('Images loaded successfully!');
+     
+    }
+  } catch (error) {
+    console.error(error);
+    showErrorMessage('An error occurred while loading images.');
+  }
+});
